e8bcd4551c5ca32148254d4ddc7de23ed5b7399b
Onengiye Onengiye <klevamane@gmail.com>
2021-05-04 21:27:09 +0100

    chore: use sqlite db

--- blog/settings.py
+++ blog/settings.py
@@ -81,16 +81,22 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": os.environ["DB_NAME"],
+#         "HOST": os.environ["DB_HOST"],
+#         "PASSWORD": os.environ["DB_PASSWORD"],
+#         "USER": os.environ["DB_USER"],
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ["DB_NAME"],
-        "HOST": os.environ["DB_HOST"],
-        "PASSWORD": os.environ["DB_PASSWORD"],
-        "USER": os.environ["DB_USER"],
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'blog/blogdb',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
