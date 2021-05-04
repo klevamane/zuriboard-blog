7bc8033fd23a594bbe9803c6c1c9a65f1bf78813
Onengiye Onengiye <klevamane@gmail.com>
2021-05-04 21:19:20 +0100

    feat: add password reset functionality

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView, PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
 from django.urls import path, include
 
 from authentication.views import SignupView, LoginView
@@ -8,5 +8,10 @@ from django.contrib.auth import views
 urlpatterns = [
     path('signup', SignupView.as_view(), name="signup"),
     path('login', LoginView.as_view(), name="login"),
-    path('logout', LogoutView.as_view(), name="logout")
+    path('logout', LogoutView.as_view(), name="logout"),
+    path('password_change', PasswordChangeView.as_view(), name="password_change"),
+    path('password_change/done/', PasswordChangeDoneView.as_view(), name="password_change_done"),
+    path('password_reset', PasswordResetView.as_view(), name="password_reset"),
+    path('password_reset/done', PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('password_reset/done/', PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
